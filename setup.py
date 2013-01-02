5ed0076028096aa72ffd04efe434ddb5f4dd7244
Jannis Leidel <jannis@leidel.info>
2013-01-02 23:07:57 +0100

    Added support for easy_install.

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from distutils.core import setup
 
 trailer_url = 'http://youtube.com/v/rC8VJ9aeB_g?hd=1&autoplay=1'
 
-if 'install' in sys.argv:
+argv = lambda x: x in sys.argv
+
+if (argv('install') or  # pip install ..
+        (argv('--dist-dir') and argv('bdist_egg'))):  # easy_install ..
     webbrowser.open_new(trailer_url)
 
 
 setup(
     name='django-unchained',
-    version='1.0',
+    version='1.0.1',
     maintainer='Jannis Leidel',
     maintainer_email='jannis@leidel.info',
     url=trailer_url)
